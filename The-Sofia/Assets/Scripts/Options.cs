a4224fb24cf8cc121a7341a011f19e4b48df69cc
LadyBugUwU <105806677+LadyBugUwU@users.noreply.github.com>
2023-03-03 21:20:26 +0100

    menu
    yooyoyoyo

--- The-Sofia/Assets/Scripts/Options.cs
+++ The-Sofia/Assets/Scripts/Options.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Audio;
 
 public class Options : MonoBehaviour
 {
+
+    public AudioMixer audioMixer; 
     public void FullScreen(bool full)
     {
         Screen.fullScreen = full;
+    }
+
+    public void Volume(float volume)
+    {
+        audioMixer.SetFloat("volume", volume); 
     }
 }
