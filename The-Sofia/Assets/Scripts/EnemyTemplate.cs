2a914d76edbd6b6dc43ca4899bb6a3531336b0fe
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-03-30 21:45:30 +0200

    making enemies
    new level2
    makinfg enemies

---
+++ The-Sofia/Assets/Scripts/EnemyTemplate.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Enemy", menuName = "Create New Enemy")]
+
+public class EnemyTemplate : ScriptableObject
+{
+    public string enemyName;
+
+}
