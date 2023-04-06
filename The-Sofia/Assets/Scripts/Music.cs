9b07b548a1528472c335f473555fa9620482a7ec
LadyBugUwU <105806677+LadyBugUwU@users.noreply.github.com>
2023-04-06 23:49:30 +0200

    f

---
+++ The-Sofia/Assets/Scripts/Music.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Music : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+    public AudioMixerSnapshot MenuSnapShot;
+    public AudioMixerSnapshot Level1SnapShot;
+
+
+
+}
