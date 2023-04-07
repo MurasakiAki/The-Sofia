4d62ad8488492994efdd4a70c24904efa4fdf8b2
LadyBugUwU <105806677+LadyBugUwU@users.noreply.github.com>
2023-04-08 00:37:41 +0200

    je to v prdeli

---
+++ The-Sofia/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] AudioMixer audioMixer;
+    public const string music_k = "musicVolume"; 
+
+
+ 
+
+
+}
--- The-Sofia/Assets/Scripts/Music.cs
+++ The-Sofia/Assets/Scripts/Music.cs
@@ -2,14 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class Music : MonoBehaviour
 {
     public AudioMixer audioMixer;
     public AudioMixerSnapshot snapshot1;
+    public Slider musicSlider; 
 
     private void Start()
     {
-        snapshot1.TransitionTo(0f);
+        snapshot1.TransitionTo(musicSlider.value);
     }
+
+  
 }
