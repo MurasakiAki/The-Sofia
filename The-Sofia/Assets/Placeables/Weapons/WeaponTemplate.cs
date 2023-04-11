18a1b49951f543e98db8cb76293a9e943ae0669a
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-04-11 17:45:44 +0200

    Added weapontemplate
    beginning weapon logic

---
+++ The-Sofia/Assets/Placeables/Weapons/WeaponTemplate.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Weapon", menuName = "Create New Weapon")]
+
+public class WeaponTemplate : ScriptableObject
+{
+    //Name
+    public string weaponName; //weapon name
+    public int id; //id of a weapon
+    public string rarity; //rarity of the weapon
+
+    //Physics
+    public int mass; //mass of a weapon
+    //sword -> light weapon
+    //spear -> heavy weapon
+    //bow -> middle
+
+    //Damage
+    public int damageRangeMin; //a minimum damage a weapon can make
+    public int damageRangeMax; //a maximum damage a weapon can make
+    public float hitRate; //hit rate of a weapon(how fast it is) 
+    
+    //Bonuses
+    public int critChance; //critical damage chance
+    public int critMultiplier;
+
+    //Visuals
+    public Sprite icon; //weapon icon in inventory/ground
+    
+}
+
