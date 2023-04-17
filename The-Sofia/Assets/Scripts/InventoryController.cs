cf19a3df2ac89fdcb87421475c949c7017ec9be5
MurasakiAki <123252526+MurasakiAki@users.noreply.github.com>
2023-04-17 20:10:44 +0200

    Summary
    - started inventory
    - added more weapons

--- The-Sofia/Assets/Scripts/InventoryController.cs
+++ The-Sofia/Assets/Scripts/InventoryController.cs
@@ -4,10 +4,30 @@ using UnityEngine;
 
 public class InventoryController : MonoBehaviour
 {
-
+    private GameObject inventoryCanvas;
     private List<GameObject> inventory = new List<GameObject>();
     private int invSize = 9;
+
+    private List<GameObject> weaponSlot = new List<GameObject>();
+    private List<GameObject> armorSlot = new List<GameObject>();
     
+    void Start()
+    {
+        inventoryCanvas = GameObject.Find("Inventory");
+    }
+
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Tab))
+        {
+            inventoryCanvas.SetActive(true);
+        }
+        else
+        {
+            inventoryCanvas.SetActive(false);
+        }
+    }
+
     public void Take(GameObject item)
     {
         if(inventory.Count != invSize)
@@ -15,13 +35,18 @@ public class InventoryController : MonoBehaviour
             inventory.Add(item);
             Debug.Log(item.name + " has been added to the inventory");
             Destroy(item.gameObject);
-            
+
         }else
         {
             //The inventory is full, so we can't add the item
             Debug.Log("Inventory is full");
         }
    
+    }
+
+    public void Equip()
+    {
+
     }
 
 }
